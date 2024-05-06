01755e279db158a75237a6797a682a44d5dd20bc
diegoascanio <6247962+DiegoAscanio@users.noreply.github.com>
2024-05-06 12:13:08 -0300

    adding windows compatibility

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+SETLOCAL
+
+REM Read each line from .env file
+for /F "tokens=1* delims==" %%i in (.env) do (
+    REM Set environment variables
+    set %%i=%%j
+)
+
+REM Build using docker-compose
+docker-compose build
+
+ENDLOCAL
